1c95634c111e4dd794ac8f61f55c9726cc725d3c
Ralph-Uwe Börner <rub@geophysik.tu-freiberg.de>
2020-02-27 11:53:33 +0100

    Add option to modify markers in plot

--- plot_model.m
+++ plot_model.m
@@ -1,9 +1,12 @@
-function plot_model(rho, thk)
+function plot_model(rho, thk, marker)
 
+if nargin < 3
+    marker = '.-';
+end
 r = reshape(repmat(reshape(rho, 1, []), 2, 1), [], 1);
 z = [0; reshape(repmat(reshape(cumsum(thk), 1, []), 2, 1), [], 1); 1e3];
 
-plot(r, z, '.-');
+plot(r, z, marker);
 ylim([0 1.4 * z(end - 1)]);
 xlabel('rho in Ohmm');
 ylabel('depth in m');
